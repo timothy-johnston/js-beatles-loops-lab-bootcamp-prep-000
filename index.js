21a648439c411dc44fd351e72460a4ade753dda5
tj11 <tedwardj11@gmail.com>
2018-03-02 18:23:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function theBeatlesPlay(musician,instrument) {
-  var sentence
+  var sentence = []
   for (var i=0;i<musician.length;i++) {
     sentence[i]=musician[i]+instrument[i]
   }
-  console.log(sentence)
+  //console.log(sentence)
   return sentence
 }
